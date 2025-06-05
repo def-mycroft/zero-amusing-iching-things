b33e305e3a606bf33a3acbcb85391b00e09b72a2
zero <dasenbrockjw@gmail.com>
2025-06-05 16:29:27 -0600

    added hexagram name mapping

--- zero_iching/helpers.py
+++ zero_iching/helpers.py
@@ -1,4 +1,15 @@
 
+HEXAGRAM_NAMES = {
+    '111': "ch'ien",     # ☰  Heaven
+    '000': "k'un",       # ☷  Earth
+    '010': "k'an",       # ☵  Water
+    '101': "li",         # ☲  Fire
+    '001': "chen",       # ☳  Thunder
+    '100': "ken",        # ☶  Mountain
+    '110': "sun",        # ☴  Wind/Wood
+    '011': "tui",        # ☱  Lake
+}
+
 
 def uuid_to_bin(uuid_str):
     """Convert UUID string to 128-bit binary string."""
